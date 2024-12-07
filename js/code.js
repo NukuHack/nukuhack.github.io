9df7b88bfa3f94a747a14b431c80991b82857404
NukuHack <nukuhack@gmail.com>
2024-12-07 17:04:34 +0100

    fetch

--- js/code.js
+++ js/code.js
@@ -4,3 +4,29 @@ apple.get("text.txt", function(data) {
     let items = data.split(';');
 });
 */
+
+let Data;
+
+// just the json fetch
+function fetchData() {
+    fetch('./json/data.json')
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error
+                (`HTTP error status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((amabatukam) => {
+            Data = amabatukam.data;
+            //console.log(Data);
+
+        })
+        .catch((error) =>{
+                console.error('Json load error:', error)
+        });
+}
+
+
+console.log("I love bread");
+
