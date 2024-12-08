cfef7e3b42a4521eae0e2bcd6887054107186fb6
NukuHack <nukuhack@gmail.com>
2024-12-08 10:28:45 +0100

    making the page better

--- js/links.js
+++ js/links.js
@@ -6,30 +6,38 @@ const linkHelper = [
         "title": "Youtube:",
         "content": "Youtube",
         "link": "https://www.youtube.com",
+        "extra":"The main page of Youtube",
     },
     {
         "id": 1,
         "title": "Facebook:",
         "content": "Facebook",
         "link": "https://www.facebook.com",
+        "extra":"The main page of Facebook",
     },
     {
         "id": 2,
         "title": "Wikipedia:",
         "content": "Wikipedia",
         "link": "https://www.wikipedia.com",
+        "extra":"The main page of Wikipedia",
     },
 ]
 
-linkHelper.forEach(({id, title, content, link = ""}, index) => {
+linkHelper.forEach(({id, title, content, link="#",extra}, index) => {
     let link_single = "";
     link_single += `
-        <li class="links_li">
-            <div class="link">
-                <p class="link_title">${title}</p>
+        <div class="link">
+            <p class="link_title">
+                ${title}
+            </p>
+            <div class="link_content">
                 <a class="link_link" href="${link}" target="_blank">${content}</a>
             </div>
-        </li>
+            <div class="link_extra">
+                ${extra}
+            </div>
+        </div>
     `;
     links.insertAdjacentHTML("beforeend", link_single);
 });
